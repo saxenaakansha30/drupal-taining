f424b0f1d09fd83d9c3c158dfa8924f8e1c39960
Akansha Saxena <akansha.saxena@acquia.com>
2023-04-01 23:05:31 +0530

    Config form

--- src/Form/ExamplesConfigurationForm.php
+++ src/Form/ExamplesConfigurationForm.php
@@ -14,7 +14,7 @@ class ExamplesConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'your_module_admin_settings';
+    return 'examples_admin_settings';
   }
 
   /**
@@ -22,7 +22,7 @@ class ExamplesConfigurationForm extends ConfigFormBase {
    */
   protected function getEditableConfigNames() {
     return [
-      'your_module.admin_settings',
+      'examples.admin_settings',
     ];
   }
 
@@ -30,22 +30,45 @@ class ExamplesConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('your_module.admin_settings');
+    $config = $this->config('examples.admin_settings');
+
     $form['your_message'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your message'),
-      '#default_value' => $config->get('variable_name'),
+      '#default_value' => $config->get('your_message'),
     ];
+
+    $form['age'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Your Age'),
+      '#default_value' => $config->get('con_age'),
+    ];
+
     return parent::buildForm($form, $form_state);
+  }
+
+ /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (strlen($form_state->getValue('your_message')) < 3) {
+      $form_state->setErrorByName('your_message', $this->t('The length  is too short. Please enter long message.'));
+    }
+
+    if ($form_state->getValue('age') < 18) {
+      $form_state->setErrorByName('age', $this->t('The age should be greater than 18'));
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->config('your_module.admin_settings')
-      ->set('variable_name', $form_state->getValue('your_message'))
+    $this->config('examples.admin_settings')
+      ->set('your_message', $form_state->getValue('your_message'))
+      ->set('con_age', $form_state->getValue('age'))
       ->save();
+
     parent::submitForm($form, $form_state);
   }
 
